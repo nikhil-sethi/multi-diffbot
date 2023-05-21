6c9fde9b48becbfc1f894369745d28018aaa5c5e
Dries <dries.borstlap@gmail.com>
2023-05-21 20:55:03 +0200

    added vision file which can output OccupancyGrid on 'map' node. Also added global nav file

--- bullproof_ws/src/bullproof-ros/scripts/global_planner.py
+++ bullproof_ws/src/bullproof-ros/scripts/global_planner.py
@@ -1,19 +1,76 @@
 #!/usr/bin/env python3
 import rospy
-from nav_msgs import OccupancyGrid
-from vision import Generate_occupancy_map
+from nav_msgs.msg import OccupancyGrid
+from std_msgs.msg import String
+import numpy as np
 
-class GlobalPlanner:
-	def __init__(self, name):
-		#super.__init__(name)
-		self.sub = rospy.Subscriber('/occupancy_map',OccupancyGrid, queue_size = 20)
-		self.name = name
-	def visualize_grid(self):
-		map = self.sub
-		for row in map:
-			print(row)
+
+# class GlobalPlanner:
+# 	def __init__(self, name):
+# 		#super.__init__(name)
+# 		self.sub = rospy.Subscriber('/occupancy_map', OccupancyGrid, queue_size = 10)
+# 		self.name = name
+# 	def visualize_grid(self):
+# 		map = self.sub
+# 		a=1
+
+
+
+# https://people.eng.unimelb.edu.au/pbeuchat/asclinic/software/ros_code_pub_and_sub_simple.html#add-the-subscriber-callback-function-for-when-messages-are-received-subscriber
+# class map_subscriber:
+
+#   def __init__(self):
+#       # Initialise a subscriber
+#       rospy.Subscriber('map', OccupancyGrid, self.subscriberCallback, queue_size=1)
+
+#   # Implement the subscriber callback (what info/data it retrieves from the topic I think?)
+#   def subscriberCallback(self, msg):
+#       # Extract the data from the message
+#       data = msg.data
+#       # Display the data
+#       rospy.loginfo("[MAPPER SUBSCRIBER!!! :D] received message with data = " + str(data))
+#       return data
+	
+
+def visualize(map,width,height):
+    map = np.reshape(map,(height,width))
+    for i, row in enumerate(map):
+        print('this is row',i,':',row)
+    
+
+
+def callback(msg):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg.data)
+    map = msg.data
+    width = msg.info.width
+    height = msg.info.width
+    visualize(map,width,height)
+	
+def get_occupancy_map():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('map', OccupancyGrid, callback)
+
+    # print(' heeeeeerrrrrreeeeee')
+    # print(sub)
+    rospy.spin()
+    
+
+
+
+
 
 if __name__ == '__main__':
-	global_planner = GlobalPlanner('global_planner')
-	global_planner.run()
+    get_occupancy_map()
+    
+    # rospy.init_node("sub_py_node")
+    
+    # rospy.loginfo("[SUBSCRIBER PY  NODE] namespace of node = " + rospy.get_namespace())
+    
+    # rospy.spin()
+    
+    
+    # sub = map_subscriber()
+    # sub.subscriberCallback()
+    
+	
 
--- bullproof_ws/src/bullproof-ros/scripts/vision.py
+++ bullproof_ws/src/bullproof-ros/scripts/vision.py
@@ -1,31 +1,76 @@
 #!/usr/bin/env python3
+import numpy as np
+
+# grid = np.zeros((5,5),dtype=int)
+# a = grid.tolist()
+# print(type(a))
+# print(' siizzeee = ', grid.size)
+# flat_grid = grid.reshape((grid.size,)) * 100
+# print(list(np.round(flat_grid)))
+# print(' aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+
 import rospy
-# from sensor_msgs import LaserScan
-# from geometry_msgs import Twist
-from nav_msgs import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid
+from std_msgs.msg import String
 
-
-
-class OccupancyMap:
-	def __init__(self, name):
-		#super.__init__(name)
-		# self.lidar_sub = rospy.Subscriber('/scan',LaserScan, queue_size = 100)
-		self.pub = rospy.Publisher('/occupancy_map', OccupancyGrid, queue_size=10)
-		self.name = name
-	def make_map(self):
-		row = [0.]*7
-		map = [row for i in range(5)]
-		map[1][1] = 1
-		map[-1][-3] = 1
-		self.pub.publish(map)
-		return map
+# class OccupancyMap:
+# 	def __init__(self, name):
+# 		#super.__init__(name)
+# 		# self.lidar_sub = rospy.Subscriber('/scan',LaserScan, queue_size = 100)
+# 		self.pub = rospy.Publisher('/occupancy_map', OccupancyGrid, queue_size=10)
+# 		self.name = name
+# 	def make_map(self):
+# 		row = [0.]*7
+# 		map = [row for i in range(5)]
+# 		map[1][1] = 1
+# 		map[-1][-3] = 1
+# 		self.pub.publish(map)
+# 		return map
 		
 
 
 
-if __name__ == '__main__':
-	occupancymap = OccupancyMap('map1')
-	map = occupancymap.make_map()
 
+# https://w3.cs.jmu.edu/spragunr/CS354_S15/labs/mapping/mapper.py
+
+
+def generate_occupancy_map():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    pub2 = rospy.Publisher('map', OccupancyGrid, queue_size=10)#, latch=True)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = "hello world %s" % rospy.get_time()
+        
+        grid_msg = OccupancyGrid()
+        # header
+        grid_msg.header.stamp = rospy.Time.now()
+        grid_msg.header.frame_id = "map"
+        
+		# info
+        grid_msg.info.resolution = 1 # Width of each grid square in meters
+        grid_msg.info.width = 10
+        grid_msg.info.height = 10
+        grid = np.zeros((10,10), dtype=int) # 10x10 grid with values between 0 and 1
+        grid[1,1] = 100
+        grid[5,8] = 100
+        
+        flat_grid = grid.reshape((grid.size)) # make into one long list
+        
+        grid_msg.data = list(flat_grid) # occupancy grid message must be list of integers between 0 and 100
+        # grid_msg.data = grid.tolist() # occupancy grid message must be list of integers between 0 and 100
+
+        rospy.loginfo(hello_str)
+        
+        pub.publish(hello_str)
+        pub2.publish(grid_msg)
+        rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        generate_occupancy_map()
+    except rospy.ROSInterruptException:
+        pass
 
 
