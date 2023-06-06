eb6e05c5495de08565caf2e0c26618b9b99a29a5
Nikhil Sethi <sethi.nirvil@gmail.com>
2023-06-06 13:01:13 +0200

    Convert clients to classes
    - increase control freq.

--- bullproof_ws/src/bullproof_nav/scripts/bull_client.py
+++ bullproof_ws/src/bullproof_nav/scripts/bull_client.py
@@ -1,68 +1,50 @@
 #!/usr/bin/env python
 
 import rospy
-import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseStamped, Pose2D
-from actionlib_msgs.msg import GoalStatusArray
+from actionlib_msgs.msg import GoalStatusArray, GoalStatus
 import math
 
-i=0
-def movebase_client():
+class BullPlanner:
+    def __init__(self) -> None:
+        rospy.init_node('bull_client_py')
+        self.wps = [Pose2D(1.2, 1.2, 0),
+                    Pose2D(0.3, 0.3, 0)]
+        self.wp_counter = 0
 
-    client = actionlib.SimpleActionClient('/bull/move_base_simple',MoveBaseAction)
-    client.wait_for_server()
-
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "/bull/base_link"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = 0.5
-    goal.target_pose.pose.orientation.w = 1.0
-
-    client.send_goal(goal)
-    wait = client.wait_for_result()
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-        return client.get_result()
-
-def goal_publish(status):
-    global i
-    if status==3:
-        wp = goals[i%len(goals)]
-        target_pose = PoseStamped()
-        target_pose.header.frame_id = "bull_tf/map"
-        target_pose.header.stamp = rospy.Time.now()
-        target_pose.pose.position.x = wp.x
-        target_pose.pose.position.y = wp.y
-
-        target_pose.pose.orientation.w = math.cos(wp.theta/2)
-        target_pose.pose.orientation.z = math.sin(wp.theta/2)
+        self.goal_pub = rospy.Publisher("bull/move_base_simple/goal", PoseStamped, queue_size=10)
         
-        goal_pub.publish(target_pose)
-        i += 1 # for next time
+        # self.farmer_pose_sub = rospy.Subscriber("farmer/gazebo/odom_gt", Odometry, self.farmer_pose_update, queue_size=10)
+        # self.farmer_pose = Pose()
+        self.goal_status_sub = rospy.Subscriber("bull/move_base/status", GoalStatusArray, self.goal_status_callback, queue_size=10)
 
-def goal_status_callback(msg:GoalStatusArray):
-    if msg.status_list:
-        status = msg.status_list[-1].status
-        goal_publish(status)
+        # self.robot_pose_sub = rospy.Subscriber("mirte/gazebo/odom_gt", Odometry, self.robot_pose_update, queue_size=10)
+        # self.robot_pose = Pose()
+        rospy.sleep(2)
+        self.goal_publish(GoalStatus.SUCCEEDED)# to get things started
 
+    def goal_publish(self, goal_status):
+        if goal_status == GoalStatus.SUCCEEDED:
+            wp = self.wps[self.wp_counter % len(self.wps)]
+            target_pose = PoseStamped()
+            target_pose.header.frame_id = "bull_tf/map"
+            target_pose.header.stamp = rospy.Time.now()
+            target_pose.pose.position.x = wp.x
+            target_pose.pose.position.y = wp.y
+
+            target_pose.pose.orientation.w = math.cos(wp.theta/2)
+            target_pose.pose.orientation.z = math.sin(wp.theta/2)
+            
+            self.goal_pub.publish(target_pose)
+            self.wp_counter += 1
+
+    def goal_status_callback(self, msg:GoalStatusArray):
+        if msg.status_list:
+            goal_status = msg.status_list[-1].status
+            self.goal_publish(goal_status)
 
 if __name__ == '__main__':
-    # try:
-    rospy.init_node('bull_client_py')
-    #     result = movebase_client()
-    #     if result:
-    #         rospy.loginfo("Goal execution dgoal_pub = rospy.Publisher("bull/move_base_simple/goal", PoseStamped, queue_size=10)
-    goals = [Pose2D(2.0, 1.0, 0),
-            Pose2D(1.0, 2.0, 0)]
 
-    goal_pub = rospy.Publisher("bull/move_base_simple/goal", PoseStamped, queue_size=10)
-    goal_status_sub = rospy.Subscriber("bull/move_base/status", GoalStatusArray, goal_status_callback, queue_size=10)
-    rospy.sleep(2)
+    farmer_planner = BullPlanner()
 
-    # just to get things started up and set and active status=1
-    goal_publish(3)
-    
     rospy.spin()
--- bullproof_ws/src/bullproof_nav/scripts/farmer_client.py
+++ bullproof_ws/src/bullproof_nav/scripts/farmer_client.py
@@ -1,69 +1,52 @@
 #!/usr/bin/env python
 
 import rospy
-import actionlib
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import PoseStamped, Pose2D
-from actionlib_msgs.msg import GoalStatusArray
+from actionlib_msgs.msg import GoalStatusArray, GoalStatus
 import math
-i=0
-def movebase_client():
 
-    client = actionlib.SimpleActionClient('/farmer/move_base_simple',MoveBaseAction)
-    client.wait_for_server()
+class FarmerPlanner:
+    def __init__(self) -> None:
+        rospy.init_node('farmer_client_py')
+        self.wps = [Pose2D(2.6, 1.2, 0),
+                Pose2D(2.6, 2.6, 1.57),
+                Pose2D(0.4, 2.6, 3.14),
+                Pose2D(0.4, 1.2, 4.71)]
+        self.wp_counter = 0
 
-    goal = MoveBaseGoal()
-    goal.target_pose.header.frame_id = "/farmer/base_link"
-    goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = 0.5
-    goal.target_pose.pose.orientation.w = 1.0
-
-    client.send_goal(goal)
-    wait = client.wait_for_result()
-    if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
-    else:
-        return client.get_result()
-
-def goal_publish(status):
-    global i
-    if status==3:
-        wp = goals[i%len(goals)]
-        target_pose = PoseStamped()
-        target_pose.header.frame_id = "farmer_tf/map"
-        target_pose.header.stamp = rospy.Time.now()
-        target_pose.pose.position.x = wp.x
-        target_pose.pose.position.y = wp.y
-
-        target_pose.pose.orientation.w = math.cos(wp.theta/2)
-        target_pose.pose.orientation.z = math.sin(wp.theta/2)
+        self.goal_pub = rospy.Publisher("farmer/move_base_simple/goal", PoseStamped, queue_size=10)
         
-        goal_pub.publish(target_pose)
-        i += 1 # for next time
+        # self.farmer_pose_sub = rospy.Subscriber("farmer/gazebo/odom_gt", Odometry, self.farmer_pose_update, queue_size=10)
+        # self.farmer_pose = Pose()
+        self.goal_status_sub = rospy.Subscriber("farmer/move_base/status", GoalStatusArray, self.goal_status_callback, queue_size=10)
 
-def goal_status_callback(msg:GoalStatusArray):
-    if msg.status_list:
-        status = msg.status_list[-1].status
-        goal_publish(status)
+        # self.robot_pose_sub = rospy.Subscriber("mirte/gazebo/odom_gt", Odometry, self.robot_pose_update, queue_size=10)
+        # self.robot_pose = Pose()
+        rospy.sleep(2)
+        self.goal_publish(GoalStatus.SUCCEEDED)# to get things started
 
+    def goal_publish(self, goal_status):
+        if goal_status == GoalStatus.SUCCEEDED:
+            wp = self.wps[self.wp_counter % len(self.wps)]
+            target_pose = PoseStamped()
+            target_pose.header.frame_id = "farmer_tf/map"
+            target_pose.header.stamp = rospy.Time.now()
+            target_pose.pose.position.x = wp.x
+            target_pose.pose.position.y = wp.y
+
+            target_pose.pose.orientation.w = math.cos(wp.theta/2)
+            target_pose.pose.orientation.z = math.sin(wp.theta/2)
+            
+            self.goal_pub.publish(target_pose)
+            self.wp_counter += 1
+
+    def goal_status_callback(self, msg:GoalStatusArray):
+        if msg.status_list:
+            goal_status = msg.status_list[-1].status
+            self.goal_publish(goal_status)
 
 if __name__ == '__main__':
-    # try:
-    rospy.init_node('farmer_client_py')
-    #     result = movebase_client()
-    #     if result:
-    #         rospy.loginfo("Goal execution dgoal_pub = rospy.Publisher("farmer/move_base_simple/goal", PoseStamped, queue_size=10)
-    goals = [Pose2D(2.6, 1.2, 0),
-            Pose2D(2.6, 2.6, 1.57),
-            Pose2D(0.4, 2.6, 3.14),
-            Pose2D(0.4, 1.2, 4.71)]
-    # i=0
-    goal_pub = rospy.Publisher("farmer/move_base_simple/goal", PoseStamped, queue_size=10)
-    goal_status_sub = rospy.Subscriber("farmer/move_base/status", GoalStatusArray, goal_status_callback, queue_size=10)
-    rospy.sleep(2)
 
-    # just to get things started up and set and active status=1
-    goal_publish(3)
-    
+    farmer_planner = FarmerPlanner()
+
     rospy.spin()
