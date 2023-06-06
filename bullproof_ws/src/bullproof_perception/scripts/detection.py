be238e4fd80423c1e471ea08192b6613d789fd1a
Tanya Spee <t.s.spee@student.tudelft.nl>
2023-06-06 11:04:01 -0700

    perception update

--- bullproof_ws/src/bullproof_perception/scripts/detection.py
+++ bullproof_ws/src/bullproof_perception/scripts/detection.py
@@ -2,7 +2,7 @@
 import numpy as np
 import cv2
 import rospy
-from apriltag_ros import AprilTagDetectionArray, AprilTagDetection
+from apriltag_ros.msg import AprilTagDetectionArray, AprilTagDetection
 from geometry_msgs.msg import Pose, Pose2D, PoseStamped
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
@@ -27,22 +27,27 @@ def callback_april_detections(msg):
     bull_pose.theta = get_yaw(msg.detections[0])
     farmer_pose.theta = get_yaw(msg.detections[1])
     mirte_pose.theta = get_yaw(msg.detections[2])
+    print('hoi')
 
+    locations_farmer_pub.publish(farmer_pose)
+    locations_mirte_pub.publish(mirte_pose)
+    locations_bull_pub.publish(bull_pose)
 
-    def get_rotation(msg):
-        global roll, pitch, yaw
-        orientation_q = msg.pose.pose.orientation
-        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
-        print
-        yaw
 
 
 def get_detections():
-    rospy.init_node('listener', anonymous=True)
-    # create subscriber
-    rospy.Subscriber('apriltag_ros/AprilTagDetectionArray', AprilTagDetectionArray,
-                     callback_april_detections)
+    rospy.init_node('get_detections_py', anonymous=True)
+    print('hoi1')
+    locations_farmer_pub = rospy.Publisher('locations/farmer', Pose2D, queue_size=1)
+    locations_mirte_pub = rospy.Publisher('locations/mirte', Pose2D, queue_size=1)
+    locations_bull_pub = rospy.Publisher('locations/bull', Pose2D, queue_size=1)
+    while not rospy.is_shutdown():
+        rospy.Subscriber('apriltag_ros', AprilTagDetectionArray, callback_april_detections)
+        rospy.sleep(1)
 
-    rospy.spin()
 
+if __name__ == '__main__':
+    try:
+        get_detections()
+    except rospy.ROSInterruptException:
+        pass
